552881ebecbf251c9667652115cf9bd5f3484579
yz1201 <15957121194@163.com>
2020-07-18 22:51:46 +0800

    config -st

---
+++ config-center-3344/src/main/java/cn/dbdj1201/player/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package cn.dbdj1201.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yz1201
+ * @date 2020-07-18 19:18
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ config-client-3355/src/main/java/cn/dbdj1201/player/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package cn.dbdj1201.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yz1201
+ * @date 2020-07-18 21:35
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
